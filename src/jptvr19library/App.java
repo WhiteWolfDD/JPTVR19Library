4d6c3598fd7db57f22b05bdb8a2c2fcf48d05bfc
pupil <pupil@ivkhk.ee>
2020-10-01 09:42:45 +0300

    Создан интерфейс управления программой

--- src/jptvr19library/App.java
+++ src/jptvr19library/App.java
@@ -5,6 +5,8 @@
  */
 package jptvr19library;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pupil
@@ -12,6 +14,44 @@ package jptvr19library;
  class App {
     public void run() {
         System.out.println("--- Библиотека ---");
-    }
+        boolean repeat = true;
+        do {
+            System.out.println("Задачи: ");
+            System.out.println("1. Добавить новую книгу");
+            System.out.println("2. Список книг");
+            System.out.println("3. Зарегистрировать читателя");
+            System.out.println("4. Список читателей");
+            System.out.println("5. Выдать книгу читателю");
+            System.out.println("6. Вернуть книгу в библиотеку");
+            System.out.println("Выберите задачу: ");
+            Scanner scanner = new Scanner(System.in);
+            String task = scanner.nextLine();
+            switch (task) {
+                case "0":
+                    System.out.println("--- Конец программы ---");
+                    break;
+                case "1":
+                    System.out.println("--- Добавить новую книгу ---");
+                    break;
+                case "2":
+                    System.out.println("--- Список книг ---");
+                    break;
+                case "3":
+                    System.out.println("--- Зарегистрировать читателя ---");
+                    break;
+                case "4":
+                    System.out.println("--- Список читателей ---");
+                    break;
+                case "5":
+                    System.out.println("--- Выдать книгу читателю ---");
+                    break;
+                case "6":
+                    System.out.println("--- Вернуть книгу в библиотеку ---");
+                    break;
+                default:
+                    System.out.println("Нет такой задачи");
+            }
+        } while (repeat);
     
+    }
 }
